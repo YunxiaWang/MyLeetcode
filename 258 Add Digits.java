5d0ff8099a5d5a1fe7a1150cc28f9c9ebe0c1ab6
YunxiaWang <497790284@qq.com>
2016-01-03 18:27:38 -0500

    Update 258 Add Digits.java

--- 258 Add Digits.java
+++ 258 Add Digits.java
@@ -6,3 +6,18 @@ public class Solution {
         return addDigits(sum);
     }
 }
+
+/* Do it without any loop/recursion in O(1) runtime:
+Digital root:
+if n=0; sum=0;
+if n%9=0; sum=9;
+else sum=n%9;
+To generalize the concept of digital roots to other bases b, one can simply change the 9 in the formula to b - 1.
+*/
+public class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}

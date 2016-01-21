dbfef2e116fb402521e2ee3771a615e7f2c85372
YunxiaWang <497790284@qq.com>
2016-01-21 01:07:41 -0500

    Update 278 First Bad Version.java

--- 278 First Bad Version.java
+++ 278 First Bad Version.java
@@ -12,3 +12,20 @@ public class Solution extends VersionControl {
         return h;
     }
 }
+/*just use simple binary search */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if (n == 1) return 1;
+        int left = 1;
+        int right = n;
+        while (left < right) { //注意边界
+             int mid = left + (right - left) / 2;
+             if (isBadVersion(mid)) {
+                 right = mid;
+             } else {
+                 left = mid + 1; //
+             }
+         }
+         return right;
+    }
+}

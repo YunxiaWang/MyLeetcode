71c4850f79853893e42d9944d4308f41bffd8909
YunxiaWang <497790284@qq.com>
2016-01-24 12:55:55 -0500

    Create 53 Maximum Subarray.java

---
+++ 53 Maximum Subarray.java
@@ -0,0 +1,12 @@
+/*dynamic programming*/
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+        int sum= nums[0]; int max= nums[0];
+        for (int i= 1; i < nums.length; i++) {
+            sum= Math.max(sum+nums[i], nums[i]);
+            max= Math.max(sum, max);
+        }
+        return max;
+    }
+}

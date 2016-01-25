da00dd8ddce38149890e141885acdbc8ff71dfc2
YunxiaWang <497790284@qq.com>
2016-01-25 16:32:01 -0500

    Update 145 Binary Tree Postorder Traversal.java

--- 145 Binary Tree Postorder Traversal.java
+++ 145 Binary Tree Postorder Traversal.java
@@ -17,3 +17,31 @@ public class Solution {
         return post;
     }
 }
+/*666
+pre-order traversal is root-left-right, and post order is left-right-root.
+modify the code for pre-order to make it root-right-left, and then reverse the output so that we can get left-right-root .
+1.Create an empty stack, Push root node to the stack.
+2.Do following while stack is not empty.
+2.1. pop an item from the stack and print it.
+2.2. push the left child of popped item to stack.
+2.3. push the right child of popped item to stack.
+3.reverse the ouput.*/
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> post= new ArrayList<>();
+        Stack<TreeNode> stack= new Stack<>();
+        Stack<Integer> reverse= new Stack<>();
+        if (root == null) return post;
+        stack.push(root);
+        while (!stack.empty()) {
+            root= stack.pop();
+            reverse.push(root.val);
+            if (root.left != null) stack.push(root.left);
+            if (root.right != null) stack.push(root.right);
+        }
+        while (!reverse.empty()) {
+            post.add(reverse.pop());
+        }
+        return post;
+    }
+}
